dd0278211383d96e45eb52e1bd3cf334a6806a46
joshbullers <josh.bullers@gmail.com>
2017-08-07 15:00:02 -0700

    First java work

---
+++ ByteShortInt/src/com/joshbullers/Main.java
@@ -0,0 +1,19 @@
+package com.joshbullers;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // width of 32
+        int myMinValue = -2_147_483_648;
+	    int myMaxValue = 2_147_483_647;
+
+	    // width of 8
+	    byte myByteValue = -128;
+
+	    // width of 16
+	    short myShortValue = 32767;
+
+	    // width of 64
+	    long myLongValue = -9_223_372_036_854_775_808L;
+    }
+}
